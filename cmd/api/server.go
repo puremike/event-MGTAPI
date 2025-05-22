ef1393ac4446773b53dc37a95baec03e4935aca2
puremike <michaelmarketing25@gmail.com>
2025-05-22 08:22:32 -0500

    feat: implemented graceful shutdown

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -14,7 +19,32 @@ func (app *application) server(mux http.Handler) error {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	app.logger.Infow("starting server on port", "port", app.config.port, "environment", app.config.env)
-	return server.ListenAndServe()
+	shutdown := make(chan error)
 
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+
+		app.logger.Infow("shutting down server", "signal", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		shutdown <- server.Shutdown(ctx)
+	}()
+
+	app.logger.Infow("starting server on port", "port", app.config.port, "environment", app.config.env)
+
+	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	if err := <-shutdown; err != nil {
+		app.logger.Errorw("server shutdown failed", "error", err)
+		return err
+	}
+
+	return nil
 }
